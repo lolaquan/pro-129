bc857be1a92844c37e03f427934a6dea9c8599ac
lolaquan <71743830+lolaquan@users.noreply.github.com>
2022-02-12 10:27:53 -0800

    Add files via upload

---
+++ scrapper.py
@@ -0,0 +1,49 @@
+from flask import request
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import requests
+import time
+import csv
+START_URL = "https://en.wikipedia.org/wiki/List_of_brown_dwarfs"
+browser = webdriver.Chrome("./chromedriver")
+page = requests.get(START_URL)
+time.sleep(10)
+headers = ["brown_dwarf", "constellation", "right_ascension", "declination", "app._mag.", "distance", "spectral_type", "mass", "radius", "discovery year"]
+planet_data = []
+new_planet_data = []
+def scrape():
+    for i in range(1, 430):
+        while True:
+            time.sleep(2)
+            soup = BeautifulSoup(browser.page_source, "html.parser")
+            current_page_num = int(soup.find_all("input", attrs={"class", "page_num"})[0].get("value"))
+def scrape_more_data(hyperlink):
+    try:
+        page = requests.get(hyperlink)
+        soup = BeautifulSoup(page.content, "html.parser")
+        temp_list = []
+        for tr_tag in soup.find_all("tr", attrs={"class": "fact_row"}):
+            td_tags = tr_tag.find_all("td")
+            for td_tag in td_tags:
+                try:
+                    temp_list.append(td_tag.find_all("div", attrs={"class": "value"})[0].contents[0])
+                except:
+                    temp_list.append("")
+        new_planet_data.append(temp_list)
+    except:
+        time.sleep(1)
+        scrape_more_data(hyperlink)
+scrape()
+for index, data in enumerate(planet_data):
+    scrape_more_data(data[5])
+    print(f"{index+1} page done 2")
+final_planet_data = []
+for index, data in enumerate(planet_data):
+    new_planet_data_element = new_planet_data[index]
+    new_planet_data_element = [elem.replace("\n", "") for elem in new_planet_data_element]
+    new_planet_data_element = new_planet_data_element[:7]
+    final_planet_data.append(data + new_planet_data_element)
+with open("final.csv", "w") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(headers)
+        csvwriter.writerows(final_planet_data)
